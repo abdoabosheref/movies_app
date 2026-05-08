ffe8fa0a4d0efb9c0d128515357231170f28d1de
Abdo <abdoabosheref@gmail.com>
2026-05-08 23:36:26 +0300

    feat : auth login email & paswword + login with google

--- lib/firebase_utils.dart
+++ lib/firebase_utils.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:movies_app/features/auth/data/models/user_model.dart';
-import 'package:movies_app/mvvm/models/my_user.dart';
 
 class FirebaseUtils {
 
